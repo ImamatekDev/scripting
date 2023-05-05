9261cb725a63f28c1822f53ebf30c8b896ab0ea3
ImamatekDev <124960572+ImamatekDev@users.noreply.github.com>
2023-05-05 14:15:22 +0700

    Update TestOFWSJScript.js

--- TestOFWSJScript.js
+++ TestOFWSJScript.js
@@ -1,77 +1,72 @@
-get = function() {
+get = ()=>{
   return {
     name: 'Test JScript',
-    GenerateScript: `function GenerateScript() {
-      injectToDb(getFormName('fnSalesOrder'), GenerateScriptForSO());
-    };`,
-    GenerateScriptForSO: `function GenerateScriptForSO() {
-return addNewLine([
+    GenerateScript: ()=>{
+      injectToDb(getFormName('fnSalesOrder'), generateScriptForSO());
+    },
+	  OtherJScriptFn: (inputArg)=>{
+        return `
+		  function runOtherJScriptFn(${inputArg}, inputJScript) {
+		    showMessage(inputJs + " - " + inputJScript)
+		  }
+	    `
+	  },
+    generateScriptForSO: ()=>{
+      return `
+        #language JScript
 
-"#language JScript",
-"// Test Scripting dengan JScript, wajib menggunakan #language JScript agar mode script berubah dari PascalScript ke JScript",
-"// Sumber : https://www.fast-report.com/public_download/fs_en.pdf",
-"/* Akhiran kode boleh ada ; ataupun tidak*/",
-'// Untuk string di JScript, wajib gunakan "(double quote)',
+        ${OtherJScriptFn('inputJs')}
 
-"",
-"// Modif dari prosedur yang ada. Saat ini belum ambil dari Parent Class (TInjector)",
-"function createSql(trans) {",
-"var",
-"  sql",
-"",
-"  sql = TjbSQL.Create(nil)",
-"  sql.database = DB",
-"  sql.transaction = trans",
-"  return sql",
-"}",
-"",
-"function runSql(qryObj, sql, execute) {",
-"  qryObj.close",
-"  qryObj.sql.text = sql",
-"  if (execute) {",
-"    qryObj.execQuery",
-"  }",
-"}",
-"/////",
-"",
-"function showData() {",
-'  showMessage(Master.SONO.AsString + " SCY")',
-"}",
-"",
-"function showQueryData() {",
-"",
-"var",
-"  dataSQL",
-"  dataSQL = createSql(tx)",
-"  try {",
-'    runSql(dataSql, "SELECT FIRST 2 SONO FROM SO", true)',
-"    while (!dataSql.eof) {",
-'      showMessage("No. SO = " + dataSql.fieldByName("SONO") )',
-"      dataSql.next",
-"    }",
-"  }",
-"  finally {",
-"    dataSQL.free",
-"  }",
-"}",
-"",
-"function loopData() {",
-"var",
-"  idxCounter",
-"const",
-"  MAX_COUNTER = 2",
-"",
-"  for (idxCounter = 0; idxCounter <= MAX_COUNTER; idxCounter++) {",
-"    if (idxCounter == 2) {",
-"      return idxCounter",
-"    }",
-"  }",
-"}",
-"",
-'Master.on_before_save_array = "showData"',
-'showMessage("Loop terakhir = " + inttostr (loopData() ) )',
-"showQueryData()"
-
-])};`,
+        function createSql(trans){
+          let sql
+          sql = TjbSQL.Create(nil)
+          sql.database = DB
+          sql.transaction = trans
+          return sql
+        }
+      
+        function runSql(qryObj, sql, execute) {
+          qryObj.close
+          qryObj.sql.text = sql
+    
+          if (execute) {
+            qryObj.execQuery
+          }
+        }
+        
+        function showData(){
+          showMessage(Master.SONO.AsString + 'SCY')
+        }
+        
+        function showQueryData(){
+          let dataSQL
+          dataSQL = createSql(tx)
+          try {
+            runSql(dataSql, 'Select first 2 SONO from SO', true)
+            while (!dataSql.eof) {
+              showMessage('No. SO = ' + dataSql.fieldByName('SONO'))
+              dataSql.next
+            }
+          } finally {
+            dataSQL.free
+          }
+        }
+        
+        function loopData(){
+          let idxCounter
+          const MAX_COUNTER = 2
+          for (idxCounter = 0; idxCounter <= MAX_COUNTER; idxCounter++){
+            if (idxCounter==2){
+              return idxCounter
+            }
+          }
+        }
+        
+        Master.on_before_save_array = "showData"
+        showMessage("Loop terakhir = " + inttostr(loopData() ) )
+        showQueryData()
+        runOtherJScriptFn("a", "b")
+      ` 
+    }
   }
 }
