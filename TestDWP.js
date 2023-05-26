9edeb5af230505bd3f0905c451af1702db7a8207
ImamatekDev <124960572+ImamatekDev@users.noreply.github.com>
2023-05-26 14:26:18 +0700

    Update TestDWP.js

--- TestDWP.js
+++ TestDWP.js
@@ -3,40 +3,12 @@ get = ()=>{
     name: 'Testing DWP',
     GenerateScript: ()=>{
       injectToDb('fnSalesOrders', generateScriptForListSO());
+//    insert script into form, list / dataset into fina
     },
-    OtherJScriptFn: (inputArg)=>{
-      return `
-        function runOtherJScriptFn(${inputArg}, inputJScript) {
-          showMessage(inputJs + " - " + inputJScript)
-        }
-      `
-    },
+    
     generateScriptForListSO: ()=>{
       return `
-        #language JScript
-
-        ${OtherJScriptFn('inputJs')}
-
-        function createSql(trans){
-          let sql
-          sql = TjbSQL.Create(nil)
-          sql.database = DB
-          sql.transaction = trans
-          return sql
-        }
-      
-        function runSql(qryObj, sql, execute) {
-          qryObj.close
-          qryObj.sql.text = sql
-    
-          if (execute) {
-            qryObj.execQuery
-          }
-        }
-        
-        function showData(){
-          showMessage("Just show this message to you")
-        }
+        showMessage("Just show this message to you")
       ` 
     }
   }
