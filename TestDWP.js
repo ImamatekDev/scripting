f264b1f2569cfb95cca779315bb64a5374a56ccb
ImamatekDev <124960572+ImamatekDev@users.noreply.github.com>
2023-05-26 13:59:04 +0700

    Create TestDWP.js
    Show message while open list SO

---
+++ TestDWP.js
@@ -0,0 +1,43 @@
+get = ()=>{
+  return {
+    name: 'Testing DWP',
+    GenerateScript: ()=>{
+      injectToDb('fnSalesOrders', generateScriptForListSO());
+    },
+    OtherJScriptFn: (inputArg)=>{
+      return `
+        function runOtherJScriptFn(${inputArg}, inputJScript) {
+          showMessage(inputJs + " - " + inputJScript)
+        }
+      `
+    },
+    generateScriptForListSO: ()=>{
+      return `
+        #language JScript
+
+        ${OtherJScriptFn('inputJs')}
+
+        function createSql(trans){
+          let sql
+          sql = TjbSQL.Create(nil)
+          sql.database = DB
+          sql.transaction = trans
+          return sql
+        }
+      
+        function runSql(qryObj, sql, execute) {
+          qryObj.close
+          qryObj.sql.text = sql
+    
+          if (execute) {
+            qryObj.execQuery
+          }
+        }
+        
+        function showData(){
+          showMessage("Just show this message to you")
+        }
+      ` 
+    }
+  }
+}
